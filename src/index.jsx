0bbb25a729e51877ff06eb9d19aaf89d902e1551
john laguerre <john.laguerre14@gmail.com>
2024-01-26 20:40:55 +0100

    Mise à jour

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import Home from "./Home"
+import Home from "./pages/Home"
 import Header from "./components/Header"
 import Error from "./components/Error"
 
