dfa4c52b59eadb0b2bc3c4a66e596163378fc887
john laguerre <john.laguerre14@gmail.com>
2024-02-05 00:44:42 +0100

    Mise à jour de mon importation de housingData

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import Banner from "../../components/Banner"
 import Card from "../../components/Card"
-import { housingData } from "../../datas/housing"
+import housingData from "../../datas/housing"
 
 function Home() {
   return (
