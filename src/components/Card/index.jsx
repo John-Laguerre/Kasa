53e6a923f7088d770a18f2979810e1ee1b0b7ed2
john laguerre <john.laguerre14@gmail.com>
2024-02-05 00:19:48 +0100

    Nouveau fichier card

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,27 +1,9 @@
-import styled from "styled-components"
-
-const CardWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  padding: 15px;
-  background-color: #f9f9fc;
-  border-radius: 30px;
-  width: 300px;
-  height: 300px;
-  transition: 200ms;
-  &:hover {
-    cursor: pointer;
-    box-shadow: 2px 2px 10px #e2e3e9;
-  }
-`
-const CardTitle = styled.span``
-
-function Card({ title }) {
+function Card({ title, image }) {
   return (
-    <CardWrapper>
-      <CardTitle>{title}</CardTitle>
-    </CardWrapper>
+    <div className="card">
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+    </div>
   )
 }
 
