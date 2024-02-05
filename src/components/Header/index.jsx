8e16f501b9dcb699bb3e0b561270d06af5fa75f0
john laguerre <john.laguerre14@gmail.com>
2024-02-05 04:50:18 +0100

    Mise à jour du fichier et header avec le code qui le Constitue

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,24 @@
+import React from "react"
+import { NavLink } from "react-router-dom"
+import "./header.scss"
+import Logo from "../../assets/LOGO.png"
+
 function Header() {
-  return <header>{}</header>
+  return (
+    <header>
+      <NavLink to="/" className="logo">
+        <img src={Logo} alt="Kara Logo" />
+      </NavLink>
+      <nav>
+        <NavLink to="/" exact activeClassName="active">
+          Acceuil
+        </NavLink>
+        <NavLink to="/About" activeClassName="active">
+          A Propos
+        </NavLink>
+      </nav>
+    </header>
+  )
 }
 
 export default Header
