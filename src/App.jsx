f549b8c733210a4a2c9297deb48cdb6d70a3e6f0
john laguerre <john.laguerre14@gmail.com>
2024-02-05 05:05:55 +0100

    Ajout de l'importation footer et lincorporer dans ma route

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home"
 import About from "./pages/About"
 import ErrorPage from "./pages/ErrorPage"
 import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="*" element={<ErrorPage />} />
         </Routes>
       </main>
+      <Footer />
     </>
   )
 }
