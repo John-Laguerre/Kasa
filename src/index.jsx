3b1a56bee4399790ea6ff983c41e84ec9a0086ec
john laguerre <john.laguerre14@gmail.com>
2024-02-05 01:23:00 +0100

    Mise à jour de mon point de rentrer de mon App

--- src/index.jsx
+++ src/index.jsx
@@ -1,21 +1,13 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import Home from "./pages/Home"
-import About from "./pages/About"
-import Header from "./components/Header"
-import ErrorPage from "./pages/ErrorPage"
+import App from "./App"
+import { BrowserRouter } from "react-router-dom"
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
-    </Router>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 )
