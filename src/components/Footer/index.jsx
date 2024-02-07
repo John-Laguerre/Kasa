de00f9215c98ec9b203bf5ac47528c89ac8558b1
john laguerre <john.laguerre14@gmail.com>
2024-02-07 11:24:43 +0100

    Modification du fichier footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,5 +1,18 @@
-function Footer() {
-  return <footer>{}</footer>
+import React from "react"
+import "./Footer.scss"
+import Logofooter from "../../assets/LOGO2.png"
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <div className="logo">
+          <img src={Logofooter} alt="Kasa Logo" />
+        </div>
+        <div className="copyright">© 2024 Kasa. All rights reserved</div>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
