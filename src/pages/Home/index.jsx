bc5fcaec4e18cc65563351ae5c382635688c61c4
john laguerre <john.laguerre14@gmail.com>
2024-02-07 02:25:32 +0100

    Mise à jour du fichier home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-import React from "react"
-import Banner from "../../components/Banner"
-import Card from "../../components/Card"
-import { getHousings } from "../../datas/Api"
-
-function Home() {
-  const housings = getHousings()
-=======
 import React, { useState, useEffect } from "react"
 import Banner from "../../components/Banner"
 import Card from "../../components/Card"
@@ -19,23 +10,17 @@ function Home() {
   useEffect(() => {
     setHousings(getHousings())
   }, [])
->>>>>>> de474ea (Mise à jour)
 
   return (
     <>
       <Banner />
       <div className="card-container">
-<<<<<<< HEAD
-        {housings.map((house) => (
-          <Card key={house.id} />
-=======
         {housings.slice(0, 6).map((housing) => (
           <Card
             key={housing.id}
             title={housing.title}
             imageUrl={housing.cover}
           />
->>>>>>> de474ea (Mise à jour)
         ))}
       </div>
     </>
