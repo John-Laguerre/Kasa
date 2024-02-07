372437fd6f02397a7b300c377ae54fc70709e67e
john laguerre <john.laguerre14@gmail.com>
2024-02-07 02:23:24 +0100

    Mise à jour du fichier App

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,11 @@
 import React from "react"
 import { Routes, Route } from "react-router-dom"
-<<<<<<< HEAD
-=======
 import { createGlobalStyle } from "styled-components"
->>>>>>> de474ea (Mise à jour)
 import Home from "./pages/Home"
 import About from "./pages/About"
 import ErrorPage from "./pages/ErrorPage"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
-
-<<<<<<< HEAD
-function App() {
-  return (
-    <>
-=======
 const GlobalStyle = createGlobalStyle`
   div {
     font-family:  'Montserrat', sans-serif;
@@ -29,7 +20,6 @@ function App() {
   return (
     <>
       <GlobalStyle />
->>>>>>> de474ea (Mise à jour)
       <Header />
       <main>
         <Routes>
