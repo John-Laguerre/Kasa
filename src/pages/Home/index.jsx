6c8aa2a3dec3171be9c32fc0f90edd5f235e7da7
john laguerre <john.laguerre14@gmail.com>
2024-02-14 14:27:23 +0100

    modification du fichier et ajout de l'importation housing

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,23 +1,18 @@
-import React, { useState, useEffect } from "react"
+import React from "react"
 import Banner from "../../components/Banner"
 import Card from "../../components/Card"
-import { getHousings } from "../../datas/Api"
-import "./Home.scss"
+import "./Home.css"
+import housing from "../../datas/housing.json"
 
 const Home = () => {
-  const [housings, setHousings] = useState([])
-
-  useEffect(() => {
-    setHousings(getHousings())
-  }, [])
-
   return (
     <>
       <Banner />
       <div className="card-container">
-        {housings.map((housing) => (
+        {housing.map((housing) => (
           <Card
             key={housing.id}
+            id={housing.id}
             title={housing.title}
             imageUrl={housing.cover}
           />
