c6bf87511d6f1bd8468b24037a5a1569ec1cabee
john laguerre <john.laguerre14@gmail.com>
2024-02-19 20:19:59 +0100

    ajout de mon composants collapse

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,13 +1,22 @@
 import React from "react"
 import "./About.css"
 import BannerImg from "../../assets/IMG2.png"
+import Collapse from "../../components/Collapse"
 
 const About = () => {
   return (
-    <div
-      className="kara-banner2"
-      style={{ backgroundImage: `url(${BannerImg})` }}
-    ></div>
+    <>
+      <div
+        className="kara-banner2"
+        style={{ backgroundImage: `url(${BannerImg})` }}
+      ></div>
+      <div className="about-content">
+        <Collapse title="Fiabilité"></Collapse>
+        <Collapse title="Respect"></Collapse>
+        <Collapse title="Service"></Collapse>
+        <Collapse title="Sécurité"></Collapse>
+      </div>
+    </>
   )
 }
 
