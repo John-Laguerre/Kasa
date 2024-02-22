6284b6a2a2d8f6c60556a7ed9238cc3fad8f19af
john laguerre <john.laguerre14@gmail.com>
2024-02-22 15:39:28 +0100

    ajout du composant location avec la code base.

---
+++ src/components/Location.jsx
@@ -0,0 +1,12 @@
+import style from '../styles/layouts/_home_location.module.scss';
+import Card from './Card';
+
+export default function Location({ data }) {
+  return (
+    <div className={style.home__location}>
+      {data.map((card) => (
+        <Card key={card.id} card={card} />
+      ))}
+    </div>
+  );
+}
