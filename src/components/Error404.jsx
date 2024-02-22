47890e9757eb13fcc2943d8625dfe257ccbb90ea
john laguerre <john.laguerre14@gmail.com>
2024-02-22 15:57:12 +0100

    Ajout de mes composant et de la page error avev la code base

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -4,23 +4,22 @@ import style from '../styles/layouts/_error.module.scss';
 import Main from './Main';
 
 export default function ErrorMessage({ error }) {
-  // Détermine un message d'erreur par défaut pour les cas non gérés
-  let errorMessage = "Oups! Quelque chose s'est mal passé.";
-
-  // Vérifie si l'erreur est une erreur de routage et ajuste le message en conséquence
+  // Avec la fonction 'isRouteErrorResponse' de react-router-dom,
+  // il vérifie si l'erreur de la route est bien une erreur
   if (isRouteErrorResponse(error)) {
-    errorMessage = "Oups! La page que vous demandez n'existe pas.";
+    return (
+      <Main>
+        <div className={style.ctaErrorMsg}>
+          {/* affiche la valeur du statut du prop 'error' */}
+          <h1 className={style.ctaErrorMsg__title}>{error.status}</h1>
+          <p className={style.ctaErrorMsg__message}>
+            Oups! La page que vous demandez n'existe pas.
+          </p>
+          <Link className={style.ctaErrorMsg__link} to={'/'}>
+            Retourner sur la page d'accueil
+          </Link>
+        </div>
+      </Main>
+    );
   }
-
-  return (
-    <Main>
-      <div className={style.ctaErrorMsg}>
-        <h1 className={style.ctaErrorMsg__title}>{error.status || 'Erreur'}</h1>
-        <p className={style.ctaErrorMsg__message}>{errorMessage}</p>
-        <Link className={style.ctaErrorMsg__link} to={'/'}>
-          Retourner sur la page d'accueil
-        </Link>
-      </div>
-    </Main>
-  );
 }
