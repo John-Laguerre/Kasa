48f35f5510af7095f62c34bc1147cd02ce6b8f58
john laguerre <john.laguerre14@gmail.com>
2024-02-23 12:38:51 +0100

    Ajout du composant main

---
+++ src/components/Main.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import style from '../styles/layouts/_home_main.module.scss';
+
+export default function Main({ children }) {
+  return <main className={style.home_main}>{children}</main>;
+}
