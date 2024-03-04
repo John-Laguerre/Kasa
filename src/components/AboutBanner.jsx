d486595d33893a005ae499aa18ba6231fd55ffe2
john laguerre <john.laguerre14@gmail.com>
2024-03-04 22:27:16 +0100

    Changement du nom de la function

--- src/components/AboutBanner.jsx
+++ src/components/AboutBanner.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import style from '../styles/layouts/_about_banner.module.scss';
 
-function HouseBanner() {
+function AboutBanner() {
   return <div className={style.about_banner}></div>;
 }
 
-export default HouseBanner;
+export default AboutBanner;
