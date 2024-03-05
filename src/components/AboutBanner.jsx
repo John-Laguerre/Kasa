b27213a2848779a485086e10977094a476a26ce9
john laguerre <john.laguerre14@gmail.com>
2024-03-05 17:30:29 +0100

    Suppression du répertoire

--- src/components/AboutBanner.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import style from '../styles/layouts/_about_banner.module.scss';
-
-function AboutBanner() {
-  return <div className={style.about_banner}></div>;
-}
-
-export default AboutBanner;
