c30891e591f8deb2070ec58677d79127cf803fc3
john laguerre <john.laguerre14@gmail.com>
2024-03-12 15:24:55 +0100

    Suppression de commentaire et modification.

--- src/components/Gallery/Card.jsx
+++ src/components/Gallery/Card.jsx
@@ -3,18 +3,15 @@ import style from '../../styles/layouts/_home_location.module.scss';
 import { Link } from 'react-router-dom';
 
 export default function Card({ card }) {
-  /* Ce slug transforme le titre en minuscules, remplace les espaces par des tirets,
-      et est utilisé pour construire l'URL de navigation.*/
-
+  // Création du slug à partir du titre pour l'URL de la page détaillée.
   const slug = card.title.toLowerCase().split(' ').join('-');
 
   return (
-    // Utilise l'élément 'Link' pour rediriger l'utilisateur lors d'un click vers une URL définssant le titre du logement concerné
     <Link to={`/house/${slug}`} className={style.home__location__card}>
       <img
-        className={style.home__location__card__img}
         src={card.cover}
         alt={card.title}
+        className={style.home__location__card__img}
       />
       <h2 className={style.home__location__card__title}>{card.title}</h2>
     </Link>
