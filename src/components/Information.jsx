b91176dcf1011545ae1dac659a9f78bc3593a1fe
john laguerre <john.laguerre14@gmail.com>
2024-03-12 15:38:31 +0100

    Suppression des commentaire.

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -15,13 +15,8 @@ function Information({
   description,
   equipments,
 }) {
-  // Divise la chaîne en utilisant ' ' comme delimiteur
   const [firstName, lastName] = nameHost.split(' ');
-  // console.log('Le prénom :', firstName);
-  // console.log('Le Nom :', lastName);
 
-  // Parcours la liste 'equipments' reçu en prop
-  // et pour chaque équipment 'e', crée une liste ('li') qui sera stocké dans la variable 'equipmentsList'
   const equipmentsList = equipments.map((e) => <li key={e}>{e}</li>);
 
   return (
@@ -29,7 +24,6 @@ function Information({
       <div className={style.ctaInformation}>
         <div className={style.ctaInformation__house}>
           <div className={style.ctaInformation__house__info}>
-            {/* Passe le prop 'titleHouse' */}
             <h1 className={style.ctaInformation__house__info__title}>
               {titleHouse}
             </h1>
@@ -55,27 +49,23 @@ function Information({
                 {lastName}
               </p>
             </div>
-            {/* Utilisation d'un opérateur ternaire ' ? ' pour conditionner selon les données reçues oui ou non */}
             <img
-              className={style.ctaInformation__profil__host__pictureHost}
-              src={pictureHost ? pictureHost : Host}
+              src={pictureHost || Host}
               alt="portrait de l'hôte"
+              className={style.ctaInformation__profil__host__pictureHost}
             />
           </div>
-          {/* Appel du composant 'RatingStars' en lui donnant le prop 'ratings' */}
           <RatingStars ratings={Number(ratings)} />
         </div>
       </div>
       <div className={styles.ctaCollapseHouse}>
         <div className={styles.ctaCollapseHouse__item}>
-          {/* Appel du composant 'Accordion' en lui passant les props demander par le composant (opérateur ternaire ) '||(ou)' un message */}
           <Collapse
             title="Description"
             content={description || 'Aucune description pour le moment'}
           />
         </div>
         <div className={styles.ctaCollapseHouse__item}>
-          {/* Appel du composant 'Accordion' en verifiant si la liste est supèrieur à 0 si oui affiche la liste des équipements si non alors affiche un message */}
           <Collapse
             title="Equipements"
             content={
